15d5c302fbced031c8d98ac01218dd44efdd7907
Anton Rikhnovets <rihnovec93@mail.ru>
2021-08-03 14:09:58 +0900

    Добавляет vuetify и простую разметку на нем

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,31 @@
 <template>
-  <div id="app">
-  </div>
+  <v-app>
+    <v-navigation-drawer app>
+
+    </v-navigation-drawer>
+
+    <v-app-bar app></v-app-bar>
+
+    <v-main>
+      <v-list>
+        <v-list-item>
+          <v-checkbox
+                  label="Todo1"
+          ></v-checkbox>
+        </v-list-item>
+        <v-list-item>
+          <v-checkbox
+                  label="Todo 2"
+          ></v-checkbox>
+        </v-list-item>
+        <v-list-item>
+          <v-checkbox
+                  label="Todo 3"
+          ></v-checkbox>
+        </v-list-item>
+      </v-list>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
